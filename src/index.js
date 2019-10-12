d28a8ffabadf59a72ea1490a2f61a9a68729cd78
radvilovich <aska_157@mail.ru>
2019-10-12 15:44:42 +0300

    feat: solve task js-edu

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
+ const withKnow = 800;
+ const withoutKnow = 1300;
+
 module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      const ka = config[focus];
+      let hours;
+      //console.log(config[focus]);
+      if(knowsProgramming === true){
+        hours = withKnow;
+      } else {
+        hours = withoutKnow;
+      }
+      //console.log(hours);
+      const result = Math.ceil(hours/ka);
+      return result;
   };
   
